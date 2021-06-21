602fbbc111f1183b7dbcf17c913c32238baa4439
example <rathishkumar.rkdo@gmail.com>
2021-06-21 17:15:23 +0530

    active admin

---
+++ app/admin/buyers.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Buyer do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :buyer_name, :phone_number, :is_active, :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token, :confirmed_at, :confirmation_sent_at
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:buyer_name, :phone_number, :is_active, :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token, :confirmed_at, :confirmation_sent_at]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
