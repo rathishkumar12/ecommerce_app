01bfecec09d999a4cab369238ec3ccb11db53706
example <rathishkumar.rkdo@gmail.com>
2021-06-22 13:38:09 +0530

    ActiveAdmin

---
+++ app/admin/pincodes.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Pincode do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :pincode, :city, :area
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:pincode, :city, :area]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ app/admin/seller_addresses.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register SellerAddress do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :seller_id, :street_name, :lat, :lon, :pincode_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:seller_id, :street_name, :lat, :lon, :pincode_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
